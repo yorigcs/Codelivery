d58127a966d13239d674b453ffe7b9d74feb2197
Yorí Galisteu <yorigcsdev@gmail.com>
2023-02-09 00:04:38 -0300

    feat: create main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+func main() {
+	fmt.Println("ok")
+
+}
